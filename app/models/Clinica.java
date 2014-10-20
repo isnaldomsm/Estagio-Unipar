f7a3dbc31124f9426c0e7eab97d6dc55190b1a92
Lucas Rafagnin <lucas_rafagnin@hotmail.com>
2014-10-20 01:08:04 -0200

    Model Clinica

---
+++ app/models/Clinica.java
@@ -0,0 +1,13 @@
+package models;
+
+import java.io.Serializable;
+
+public class Clinica implements Serializable{
+
+	private static final long serialVersionUID = 8839076992892087197L;
+	private String nome;
+	private String email;
+	private String telefone;
+	private String cnpj;
+	
+}
