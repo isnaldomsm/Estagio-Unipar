460827d10665690402c7ec6bad841e5fe2a6b58d
Lucas Rafagnin <lucas_rafagnin@hotmail.com>
2014-10-19 22:02:27 -0200

    Adicionado os arquivos de listagem de funcionarios e financeiro

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,14 +2,14 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.financeiro;
 import views.html.header;
+import views.html.listar_funcionarios;
 import views.html.menu;
 
 public class Application extends Controller {
- 
-    public static Result index() {
-		return ok(financeiro.render(menu.render(), header.render()));
+
+	public static Result index() {
+		return ok(listar_funcionarios.render(menu.render(), header.render()));
 	}
 
 }
