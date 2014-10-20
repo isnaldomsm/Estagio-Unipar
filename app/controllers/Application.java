8ff359e4af92c706b1ba5f685e2c5f7feaeb0b33
Lucas Rafagnin <lucas_rafagnin@hotmail.com>
2014-10-19 22:16:10 -0200

    add listagem de pacientes

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,14 +3,14 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.header;
-import views.html.listar_funcionarios;
 import views.html.listar_medico;
+import views.html.listar_paciente;
 import views.html.menu;
 
 public class Application extends Controller {
 
 	public static Result index() {
-		return ok(listar_medico.render(menu.render(), header.render()));
+		return ok(listar_paciente.render(menu.render(), header.render()));
 	}
 
 }
