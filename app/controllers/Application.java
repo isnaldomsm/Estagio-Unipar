2ff299f0148c8ff2c044c03a8c5f8b318edf0511
Lucas Rafagnin <lucas_rafagnin@hotmail.com>
2014-10-19 22:10:16 -0200

    criado a listagem de medicos

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,12 +4,13 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.header;
 import views.html.listar_funcionarios;
+import views.html.listar_medico;
 import views.html.menu;
 
 public class Application extends Controller {
 
 	public static Result index() {
-		return ok(listar_funcionarios.render(menu.render(), header.render()));
+		return ok(listar_medico.render(menu.render(), header.render()));
 	}
 
 }
