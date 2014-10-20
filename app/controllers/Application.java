f94f69b68033b62d2cf4e65aef22a3b723197903
Lucas Rafagnin <lucas_rafagnin@hotmail.com>
2014-10-19 22:19:07 -0200

    add listagem de planos

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,12 +5,13 @@ import play.mvc.Result;
 import views.html.header;
 import views.html.listar_medico;
 import views.html.listar_paciente;
+import views.html.listar_planos;
 import views.html.menu;
 
 public class Application extends Controller {
 
 	public static Result index() {
-		return ok(listar_paciente.render(menu.render(), header.render()));
+		return ok(listar_planos.render(menu.render(), header.render()));
 	}
 
 }
