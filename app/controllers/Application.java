7c110e8ef1cb440ef9905c4ecaaa06d4d200f30b
Lucas Rafagnin <lucas_rafagnin@hotmail.com>
2014-10-19 22:39:14 -0200

    adicionado o cadastro de medicos

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,15 +3,13 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.header;
-import views.html.listar_medico;
-import views.html.listar_paciente;
-import views.html.listar_planos;
+import views.html.medico;
 import views.html.menu;
 
 public class Application extends Controller {
 
 	public static Result index() {
-		return ok(listar_planos.render(menu.render(), header.render()));
+		return ok(medico.render(menu.render(), header.render()));
 	}
 
 }
