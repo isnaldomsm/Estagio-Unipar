579cfef36b3c55b634671b3a7d11d761e5b90a6e
Lucas Rafagnin <lucas_rafagnin@hotmail.com>
2014-10-20 09:36:57 -0200

    Alteracoes no model Clinica, e persistencia dos dados no banco de dados h2

--- app/controllers/ClinicaController.java
+++ app/controllers/ClinicaController.java
@@ -1,16 +1,24 @@
 package controllers;
 
+import java.util.List;
+
+import models.Clinica;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.configurar_clinica;
 import views.html.header;
 import views.html.menu;
 
-public class ClinicaController extends Controller{
+public class ClinicaController extends Controller {
 
 	public static Result index() {
+		List<Clinica> clinicas = Clinica.find.findList();
+		System.out.println("Numero de clinicas: " + clinicas.size());
 		return ok(configurar_clinica.render(menu.render(), header.render()));
 	}
 
-	
+	public static Result createOrUpdate() {
+		return TODO;
+	}
+
 }
--- app/models/Clinica.java
+++ app/models/Clinica.java
@@ -1,13 +1,40 @@
 package models;
 
-import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
-public class Clinica implements Serializable{
+import play.data.validation.Constraints;
+import play.db.ebean.Model;
 
-	private static final long serialVersionUID = 8839076992892087197L;
+@Entity
+public class Clinica extends Model {
+
+	private static final long serialVersionUID = -7258390397160266610L;
+	@Id
+	private Long id;
+	@Constraints.Required
 	private String nome;
-	private String email;
-	private String telefone;
-	private String cnpj;
-	
+
+	public static Model.Finder<Long, Clinica> find = new Model.Finder<Long, Clinica>(Long.class, Clinica.class);
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		return getNome();
+	}
 }
